93e3f828afa92172e0ba4c94ec1314ac97fe7cf0
emanuele <emattina97@gmail.com>
2021-03-12 16:45:33 +0100

    Carico prova dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
 $(document).ready(function () {
     var aDrop = $('.nav-right ul li');
-    var divDrop = $('.nav-right ul div');
 
-    aDrop.mouseover(function () { 
+    aDrop.hover(function () { 
         // console.log($(this).siblings());
-        $(this).children('div').addClass('active')
+        $(this).children('.dropdown').toggleClass('active');
     });
 
-    aDrop.mouseleave(function () { 
-        // console.log($(this).siblings());
-        $(this).children('div').removeClass('active')
-    });
+    aDrop.hover(function() {
+        var isHovered = $(this).is(":hover");
+        if (isHovered) {
+          $(this).children("div").stop().slideDown(300);
+        } else {
+          $(this).children("div").stop().slideUp(300);
+        }
+      });
+
+    // aDrop.mouseover(function () { 
+    //     // console.log($(this).siblings());
+    //     $(this).children('.dropdown').addClass('active')
+    // });
+
+    // aDrop.mouseleave(function () { 
+    //     // console.log($(this).siblings());
+    //     $(this).children('.dropdown').removeClass('active')
+    // });
 
 });
 
