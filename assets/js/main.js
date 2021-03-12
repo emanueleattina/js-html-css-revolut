245d18808af3d6ac633dca7addd027f595eb8ad5
emanuele <emattina97@gmail.com>
2021-03-12 16:33:27 +0100

    Carico mouseover e mouseleave funzionante

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
-
 $(document).ready(function () {
-    document.getElementById('prova').innerHTML = "provaaaaaaaa";
+    var aDrop = $('.nav-right ul li');
+    var divDrop = $('.nav-right ul div');
+
+    aDrop.mouseover(function () { 
+        // console.log($(this).siblings());
+        $(this).children('div').addClass('active')
+    });
+
+    aDrop.mouseleave(function () { 
+        // console.log($(this).siblings());
+        $(this).children('div').removeClass('active')
+    });
+
 });
-
-
 
